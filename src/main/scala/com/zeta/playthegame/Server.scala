71634fbf58dc5019ede4e61d0b73fa84aca51520
zeta <jzelayeta@tiendanube.com>
2019-06-17 15:29:15 -0300

    custom banner

--- src/main/scala/com/zeta/playthegame/Server.scala
+++ src/main/scala/com/zeta/playthegame/Server.scala
@@ -13,6 +13,17 @@ import scala.concurrent.ExecutionContext
 
 object Server {
 
+  val playTheGameBanner =
+    """
+      |______ _       _____   _______ _   _  _____ _____   ___  ___  ___ _____
+      || ___ \ |     / _ \ \ / /_   _| | | ||  ___|  __ \ / _ \ |  \/  ||  ___|
+      || |_/ / |    / /_\ \ V /  | | | |_| || |__ | |  \// /_\ \| .  . || |__
+      ||  __/| |    |  _  |\ /   | | |  _  ||  __|| | __ |  _  || |\/| ||  __|
+      || |   | |____| | | || |   | | | | | || |___| |_\ \| | | || |  | || |___
+      |\_|   \_____/\_| |_/\_/   \_/ \_| |_/\____/ \____/\_| |_/\_|  |_/\____/
+      |
+    """.stripMargin.split("\n").toList
+
   private lazy val poolSize = ConfigFactory.defaultApplication().getConfig("mongodb").getInt("poolSize")
 
   implicit lazy val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(poolSize)) //Exclusive ExecutionPool for Database access only
@@ -23,6 +34,7 @@ object Server {
 
   def server(implicit T: Timer[IO], C: ContextShift[IO]) =
     BlazeServerBuilder[IO]
+      .withBanner(playTheGameBanner)
       .bindHttp(8080, "0.0.0.0")
       .withHttpApp(httpApp)
       .serve
